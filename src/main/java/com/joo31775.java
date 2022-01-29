5b4798c0e6b351201521cf2bb5ad0746ec35a9f4
Big_Plate <great9744@gmail.com>
2022-01-29 19:20:55 +0900

    I think I found some keys.

--- src/main/java/com/joo31775.java
+++ src/main/java/com/joo31775.java
@@ -1,25 +1,52 @@
 package com;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.GameRule;
+import org.bukkit.World;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
-
-public final class joo31775 extends JavaPlugin {
-
+public final class joo31775 extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
+        getLogger().info("server started.");
+    }
+    First ban1 = new First();
 
-        First fun = new First();
-        fun.first();
+}
 
-        Second fun2 = new Second();
-        fun2.second();
 
-        Third fun3 = new Third();
-        fun3.setting();
-
-        Third fun4 = new Third();
-
+class First extends JavaPlugin{
+    public void onEnable() {
+        getLogger().warning("Hello, World!");
     }
 }
+
+
+class Second extends JavaPlugin {
+    public void onEnable() {
+        World world = getServer().getWorld("world");
+        world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+    }
+}
+
+
+class Third extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        Bukkit.getServer().getPluginManager().registerEvents(this, this); // using listener
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendActionBar(Component.text("Welcome",(NamedTextColor.BLUE)));
+    }
+}
+
+
