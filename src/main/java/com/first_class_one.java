5185a387f6a22f5bd0756fdc554408c1c526f497
Big_Plate <great9744@gmail.com>
2022-02-20 20:39:38 +0900

    I found what the matter had been wrong. It seems to be due to "two extends JavaPlugin". I grew up!

--- src/main/java/com/first_class_one.java
+++
@@ -1,9 +0,0 @@
-package com;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class first_class_one extends JavaPlugin  {
-    public void onEnable() {
-        getLogger().warning("Hello, World!");
-    }
-}
--- src/main/java/com/joo31775.java
+++ src/main/java/com/joo31775.java
@@ -1,43 +1,31 @@
 package com;
 
 import net.kyori.adventure.text.Component;
-
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.java.JavaPlugin;
 
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
 
 
 
 public class joo31775 extends JavaPlugin implements Listener {
 
     public void onEnable() {
-        getLogger().warning("Server started");
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().warning("Server started.");
 
-        getLogger().info("a"); // check point 1
+        first_function first = new first_function(); // An error occurred that starts from here.
+        first.onEnable();
 
-        testing test = new testing(); // An error occurred that starts from here.
-        test.onEnable();
 
-        getLogger().info("b"); // check point 2
-
-        first_class_one aaaa = new first_class_one();
-        aaaa.onEnable();
-
-        getLogger().info("c"); // check point 3
+        getLogger().info("Nice try!"); // check point
     }
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().sendActionBar(Component.text("content",(NamedTextColor.BLUE)));
-    }
-
 }
 
-class testing extends JavaPlugin {
-    public void onEnable() {
-        getLogger().warning("Hello!");
+class first_function implements Listener {
+    void onEnable() {
+        String ababa = "1";
     }
 }
